6c6f5c1b7156ae1100fa29dfcacc35d550946579
Mahnoor-Muhammad-Naeem <mahnoormuhammadnaeem99@gmail.com>
2025-10-27 16:00:09 +0500

    feat: add React tutorials (tut01–tut15) and setup

---
+++ tut07/src/App.js
@@ -0,0 +1,18 @@
+// REACT JS LISTS AND KEYS:
+// npm install react-icons@4 => FOR Content.js file: import { FaTrashAlt } from 'react-icons/fa';
+
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
